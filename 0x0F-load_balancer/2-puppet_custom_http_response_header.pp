2c625becf604a018c5d2a7b7782e1afc5decd268
Hiba <hallomer@Hibas-MacBook-Pro.local>
2024-05-14 05:21:33 +0300

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,24 @@
-# Creating a custom HTTP header response, but with Puppet.
+# Configure Nginx to have a custom HTTP response header
 
 package { 'nginx':
   ensure => installed,
-}   
-
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https:\/\/www.youtube.com\/watch?v=Htqn7tGvQsc permanent;',
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
-
 file_line { 'Add X-Served-By header':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => "        add_header X-Served-By ${::hostname};",
-  notify => Service['nginx']
+  line   => "add_header X-Served-By ${::hostname};",
+  notify => Service['nginx'],
 }
 
 service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Add X-Served-By header'],
 }
