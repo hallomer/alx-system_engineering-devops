fe3d369037a8ae1589bf75a13abd43ed96e49173
Hiba <hallomer@Hibas-MacBook-Pro.local>
2024-05-14 04:54:28 +0300

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,17 @@
 # Configure Nginx to have a custom HTTP response header
-
 package { 'nginx':
-  ensure => installed,
+  ensure => 'present'
 }
 
-file_line { 'custom_http_response_header':
-  ensure => present,
+file_line { 'Add X-Served-By header':
+  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => '        add_header X-Served-By $hostname;',
-  notify => Service['nginx'],
+  line   => "        add_header X-Served-By ${::hostname};",
+  notify => Service['nginx']
 }
 
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+  ensure => 'running',
+  enable => 'true'
 }
