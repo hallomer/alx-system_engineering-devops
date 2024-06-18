b76b6e02e44619446d59a23b6c1dbb46758d9b96
hiba <hebaaltayeb2@icloud.com>
2024-06-19 00:43:39 +0300

    how bout now

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0\
+               (by /u/halomer)"}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
-    data = response.json().get("data")
+
+    data = response.json().get("data", {})
     return data.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "python:subreddit.top.ten:v1.0 (by /u/halomer)"}
     params = {'limit': 10}
     response = requests.get(url, headers=headers,
                             params=params, allow_redirects=False)
