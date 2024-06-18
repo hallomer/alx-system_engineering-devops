f366f04ba536f8b5a01bfa50c4d11e73f33d47de
hiba <hebaaltayeb2@icloud.com>
2024-06-19 00:50:42 +0300

    how bout now

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0\
-               (by /u/halomer)"}
-
+    headers = {'User-Agent': 'custom-agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
-
-    data = response.json().get("data", {})
-    return data.get("subscribers")
+    data = response.json().get('data', {})
+    return data.get('subscribers', 0)
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,13 +7,14 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "python:subreddit.top.ten:v1.0 (by /u/halomer)"}
-    params = {'limit': 10}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
+    headers = {'User-Agent': 'custom-agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print("None")
         return
-    data = response.json().get("data", {}).get("children", [])
+    data = response.json().get('data', {}).get('children', [])
+    if not data:
+        print("None")
+        return
     for post in data:
-        print(post.get("data", {}).get("title"))
+        print(post.get('data', {}).get('title', "None"))
