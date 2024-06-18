74f75958cb95f6ed20ae4495a275cc14ba43aa36
hiba <hebaaltayeb2@icloud.com>
2024-06-19 00:25:19 +0300

    0

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,12 +8,12 @@ def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
     if response.status_code != 200:
         print("None")
         return
-
     data = response.json().get("data", {}).get("children", [])
     for post in data:
         print(post.get("data", {}).get("title"))
