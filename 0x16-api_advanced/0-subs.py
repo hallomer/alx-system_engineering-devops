6fd532ecccbc5f0773259ed265aa7b317ffb3943
hiba <hebaaltayeb2@icloud.com>
2024-06-18 23:55:41 +0300

    whats wrong

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Custom'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        subs = data.get('data').get('subscribers')
-        return subs
-    else:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
+
+    data = response.json().get("data", {})
+    return data.get("subscribers", 0)
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,15 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers='Custom', params={"limit": "10"})
+    """Prints the titles of the first 10 hot posts."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-    if response.status_code == 200:
-        for get_data in response.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
-    else:
-        print(None)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+    for post in data:
+        print(post.get("data", {}).get("title"))
