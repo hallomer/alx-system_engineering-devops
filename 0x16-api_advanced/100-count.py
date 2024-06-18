4589570a627d4c38c6d550bbfda1aa566d463916
hiba <hebaaltayeb2@icloud.com>
2024-06-18 23:36:14 +0300

    pycodestyle

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,13 +2,15 @@
 """Queries the Reddit API."""
 import requests
 
+
 def count_words(subreddit, word_list, hot_list=[], after=''):
     """Parses the title of all hot articles, and prints a sorted count."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after, 'limit': 100}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+
     if response.status_code != 200:
         return
 
@@ -30,7 +32,8 @@ def count_words(subreddit, word_list, hot_list=[], after=''):
             word_lower = word.lower()
             word_count[word_lower] += title.split().count(word_lower)
 
-    sorted_word_count = sorted(word_count.items(), key=lambda item: (-item[1], item[0]))
+    sorted_word_count = sorted(word_count.items(),
+                               key=lambda item: (-item[1], item[0]))
 
     for word, count in sorted_word_count:
         if count > 0:
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,23 +2,24 @@
 """Queries the Reddit API."""
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=''):
     """Returns a list containing the titles of all hot articles."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after, 'limit': 100}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+
     if response.status_code != 200:
         return None
-    
+
     data = response.json().get('data', {})
     posts = data.get('children', [])
     for post in posts:
         hot_list.append(post.get('data', {}).get('title', None))
-    
+
     after = data.get('after', None)
     if after:
         return recurse(subreddit, hot_list, after)
     return hot_list
-    
