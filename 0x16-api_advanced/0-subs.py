b52e5684614d9458794aa8a81e212b420a54c01e
hiba <hebaaltayeb2@icloud.com>
2024-06-18 23:46:28 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Custom'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subs = data['data']['subscribers']
+        subs = data.get('data').get('subscribers')
         return subs
     else:
         return 0
