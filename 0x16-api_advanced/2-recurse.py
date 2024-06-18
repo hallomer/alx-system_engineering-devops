63efcf0ef9bef9e229f1e3bfdd0fd55893917c2f
hiba <hebaaltayeb2@icloud.com>
2024-06-19 00:56:32 +0300

    whatever

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ import requests
 def recurse(subreddit, hot_list=[], after=''):
     """Returns a list containing the titles of all hot articles."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'custom-agent'}
     params = {'after': after, 'limit': 100}
     response = requests.get(url, headers=headers,
                             params=params, allow_redirects=False)
