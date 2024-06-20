edb8ca08e581e1489e45cd0fdcb2b78fd0d1b462
hiba <hebaaltayeb2@icloud.com>
2024-06-20 21:20:43 +0300

    bye

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,3 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         return response.json()['data']['subscribers']
     return 0
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
