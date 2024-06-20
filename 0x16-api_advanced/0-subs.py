377f73327a6c355fefcf78b68c35bd63c1abb2f5
hiba <hebaaltayeb2@icloud.com>
2024-06-20 20:54:42 +0300

    please

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,19 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom-agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        return 0
-
-    try:
-        data = response.json()
-    except Exception:
-        return 0
-
-    if data is None or 'subscribers' not in data:
-        return 0
-
-    return data.get('subscribers')
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'ALXtask/0.0.1'}
+    request = requests.get(url, allow_redirects=False, headers=headers)
+    if request.status_code == 200:
+        return request.json().get('data').get('subscribers')
+    return 0
