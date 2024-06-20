87d7c5fd7d8c864b24ea119f87001e03ca32254d
hiba <hebaaltayeb2@icloud.com>
2024-06-20 20:35:58 +0300

    what

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """Queries the Reddit API."""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
-
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "Users-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/halomer)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
