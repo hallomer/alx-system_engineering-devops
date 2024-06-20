a593ad12e3970b72d03619d35f3508971f064693
hiba <hebaaltayeb2@icloud.com>
2024-06-20 20:44:35 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "Users-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/halomer)"
-    }
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom-agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    
+    if response.status_code != 200:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    
+    data = response.json().get('data')
+    return data.get('subscribers')
+
+
+if __name__ == "__main__":
+    import sys
+    subreddit = sys.argv[1]
+    print(number_of_subscribers(subreddit))
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """Queries the Reddit API."""
-
 import requests
 
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts."""
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
-    else:
-        print(None)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'custom-agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
+    data = response.json().get('data').get('children')
+    if not data:
+        print("None")
+        return
+    for post in data:
+        print(post.get('data').get('title'))
