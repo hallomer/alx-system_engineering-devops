c6236ad15aaa8ad9d98fd31c2f7b3303e8210cca
hiba <hebaaltayeb2@icloud.com>
2024-06-20 20:27:20 +0300

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,13 +8,22 @@ def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'custom-agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        if not posts:
+            print("None")
+        for post in posts:
+            print(post.get('data', {}).get('title', "None"))
+    except (requests.RequestException, ValueError):
         print("None")
-        return
-    data = response.json().get('data', {}).get('children', [])
-    if not data:
-        print("None")
-        return
-    for post in data:
-        print(post.get('data', {}).get('title', "None"))
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
