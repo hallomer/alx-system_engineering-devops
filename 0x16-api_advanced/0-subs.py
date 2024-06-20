4d1a2a457988c22afe405a7cd25500fba72e259e
hiba <hebaaltayeb2@icloud.com>
2024-06-20 21:00:54 +0300

    last time

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers."""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'ALXtask/0.0.1'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'subscribers/0.1'}
     request = requests.get(url, allow_redirects=False, headers=headers)
     if request.status_code == 200:
         return request.json().get('data').get('subscribers')
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,15 +5,13 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'custom-agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print("None")
-        return
-    data = response.json().get('data').get('children')
-    if not data:
-        print("None")
-        return
-    for post in data:
-        print(post.get('data').get('title'))
+    url = 'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'ALXtask/0.0.1'}
+    params = {'limit': 10}
+    request = requests.get(url, allow_redirects=False, headers=headers,
+                           params=params)
+    if request.status_code == 200:
+        for post in request.json().get('data').get('children'):
+            print(post.get('data').get('title'))
+    else:
+        print(None)
