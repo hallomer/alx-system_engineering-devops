6919a8ea27d6861f2ad80859dc02f87ebe8e15a6
hiba <hebaaltayeb2@icloud.com>
2024-06-27 07:53:41 +0300

    debug1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Adjusts the limits for holberton user
+
+exec { 'hard-limit':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 'soft-limit':
+  command => "sed -i '/^holberton soft/s/4/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/',
+}
